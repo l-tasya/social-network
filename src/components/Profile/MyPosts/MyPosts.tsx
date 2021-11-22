d31b371a578e99cb3ae1d71b7ab1ff2401e2e7bf
l-tasya <dalionfull@gmail.com>
2021-11-22 19:21:26 +0600

    add input error

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from './MyPosts.module.scss'
 import Post from './Post/Post';
 import {Plus} from 'react-feather';
@@ -19,14 +19,22 @@ type MyPostsPropsType = {
 
 const MyPosts: React.FC<MyPostsPropsType> = ({state, addPost, newPostText,changeInput}) => {
     //converting post state to Post component
+    let [error, setError] = useState<'required field'|''>('')
     let postElements = state.map((t) => <Post key={t.id} message={t.message} img={t.src}/>)
 
     //Event Handlers
     let onAddPostButtonClick = (e: any) => {
-        addPost(2);
+        if (newPostText.trim() !== '') {
+            addPost(2)
+            setError('')
+        } else {
+            setError('required field')
+        }
+
 
     }
     let onEnterKeyPressHandler = (e: React.KeyboardEvent) => {
+        setError('')
         if (e.key === 'Enter' && !e.shiftKey)
         {
             e.preventDefault();
@@ -41,7 +49,6 @@ const MyPosts: React.FC<MyPostsPropsType> = ({state, addPost, newPostText,change
     let onTextAreaChangeHandler = (event: ChangeEvent<HTMLTextAreaElement>) => {
         changeInput(`${event.currentTarget.value}`)
     }
-    //todo: if value of texArea null = dont add post
     return (
         <div className={s.myPosts}>
             <div className={s.myPosts__add}>
@@ -60,8 +67,10 @@ const MyPosts: React.FC<MyPostsPropsType> = ({state, addPost, newPostText,change
                          placeholder={`What's on your mind?`}
                          cols={30}
                          rows={10}
+                         className={error === 'required field'?s.error:''}
                      >
                      </textarea>
+                    {error && <span className={s.errorMessage}>{error}</span>}
                 </div>
             </div>
             <div className={s.myPosts__posts}>
