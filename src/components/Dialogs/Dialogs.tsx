0c983ef4dbf03b52cbaf80757a790f506b589755
l-tasya <dalionfull@gmail.com>
2021-12-03 20:33:45 +0600

    input of add message

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.scss';
-import Message from "./Message/Message";
-import DialogsItem from "./DialogsItem/DialogsItem";
+import Message from './Message/Message';
+import DialogsItem from './DialogsItem/DialogsItem';
+import {ArrowRight, MessageCircle} from 'react-feather';
 
 type DialogsType = {
     name: string
@@ -10,13 +11,17 @@ type DialogsType = {
 type MessageType = {
     id: number
     message: string
+    sent: boolean
 }
 type DialogsPageType = {
     dialogs: Array<DialogsType>
     messages: Array<MessageType>
+    messageCurrentValue: string
 }
 type DialogsPropsType = {
     state: DialogsPageType
+    changeMessage: (text: string) => void
+    sendMessage: ()=>void
 }
 
 
@@ -24,14 +29,24 @@ const Dialogs: React.FC<DialogsPropsType> = (props) => {
     //converting dialog state to DialogItem component
     let dialogElements = props.state.dialogs.map((t) => <DialogsItem key={t.id} name={t.name} id={t.id}/>)
     //converting message state from props, to Message component
-    let messageElements = props.state.messages.map((t) => <Message key={t.id} text={t.message}/>)
+    let messageElements = props.state.messages.map((t) => <Message sent={t.sent} key={t.id} text={t.message}/>)
+
+    const onMessageInputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        props.changeMessage(e.currentTarget.value)
+    }
     return (
         <div className={s.dialogs}>
             <div className={s.dialogs__items}>
+                <MessageCircle className={s.dialogIcon}/>
                 {dialogElements}
             </div>
-            <div className={s.messages}>
+            <div className={s.dialogs__messages}>
                 {messageElements}
+                <div className={s.dialogs__typing}><input value={props.state.messageCurrentValue}
+                                                          className={s.dialogs__input} type="text"
+                                                          placeholder={'Start typing..'}
+                                                          onChange={onMessageInputChangeHandler}/><span
+                    className={s.dialogs__send}><ArrowRight onClick={props.sendMessage}/></span></div>
             </div>
         </div>
     )
