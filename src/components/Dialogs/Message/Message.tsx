6e84abc2d4305b6bd34afe5100318f69a6a5629c
l-tasya <dalionfull@gmail.com>
2021-12-03 20:34:40 +0600

    add className logic

--- src/components/Dialogs/Message/Message.tsx
+++ src/components/Dialogs/Message/Message.tsx
@@ -3,10 +3,11 @@ import s from '../Dialogs.module.scss';
 
 type MessagePropsType = {
     text: string,
+    sent: boolean,
 }
 
 const Message = (props: MessagePropsType) => {
-    return <div className={s.message}>{props.text}</div>
+    return <span className={(props.sent?s.senDED:null) + ' ' + s.message}>{props.text}</span>
 
 }
 export default Message;
