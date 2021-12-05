f0f3abb0d96ea74a327869b048db20754d075b1a
l-tasya <dalionfull@gmail.com>
2021-12-05 17:10:57 +0600

    fix callbacks

--- src/index.tsx
+++ src/index.tsx
@@ -3,25 +3,26 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {addPost, onMessageChange, onPostInputChange, RootStateType, sendMessage, state, subscribe} from './redux/state';
+import {RootStateType, store} from './redux/state';
+
 
 let rerenderEntireTree = (state: RootStateType,)=>{
     ReactDOM.render(
         <React.StrictMode>
             <App
                 state={state}
-                changePosts={addPost}
-                changeInput={onPostInputChange}
-                changeMessage={onMessageChange}
-                sendMessage={sendMessage}
+                changePosts={store.addPost.bind(store)}
+                changeInput={store.onPostInputChange.bind(store)}
+                changeMessage={store.onMessageChange.bind(store)}
+                sendMessage={store.sendMessage.bind(store)}
 
             />
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(state)
-subscribe(()=>{rerenderEntireTree(state)})
+rerenderEntireTree(store.getState())
+store.subscribe(()=>{rerenderEntireTree(store.getState())})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
