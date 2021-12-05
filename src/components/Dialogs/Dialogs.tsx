c09cb3b2436959b11f94e77362443a632308cce0
l-tasya <dalionfull@gmail.com>
2021-12-05 15:06:50 +0600

    add input error

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,4 +1,4 @@
-import React, {ChangeEvent} from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import s from './Dialogs.module.scss';
 import Message from './Message/Message';
 import DialogsItem from './DialogsItem/DialogsItem';
@@ -26,21 +26,27 @@ type DialogsPropsType = {
 
 
 const Dialogs: React.FC<DialogsPropsType> = (props) => {
-    //converting dialog state to DialogItem component
+    //local state
+    let [error, setError] = useState<string|null>(null);
+    //converting state to component
     let dialogElements = props.state.dialogs.map((t) => <DialogsItem key={t.id} name={t.name} id={t.id}/>)
-    //converting message state from props, to Message component
     let messageElements = props.state.messages.map((t) => <Message sent={t.sent} key={t.id} text={t.message}/>)
 
+    //event handlers
     const onMessageInputChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
-
+        setError('')
         props.changeMessage(e.currentTarget.value)
     }
-     const onSentButtonClick = () =>{
-         if(props.state.messageCurrentValue.trim() !== ''){props.sendMessage()}
-     }
     const enterKeyDownHandler = (e: React.KeyboardEvent) =>{
-
         e.key === "Enter" && onSentButtonClick()
+    }
+    //callback
+    const onSentButtonClick = () =>{
+        if(props.state.messageCurrentValue.trim() === ''){
+            setError('required field');
+        }else{
+            props.sendMessage()
+        }
     }
     return (
         <div className={s.dialogs}>
@@ -51,12 +57,13 @@ const Dialogs: React.FC<DialogsPropsType> = (props) => {
             <div className={s.dialogs__messages}>
                 {messageElements}
                 <div className={s.dialogs__typing}><input value={props.state.messageCurrentValue}
-                                                          className={s.dialogs__input} type="text"
+                                                          className={(error&&s.errorInput) + ' ' + s.dialogs__input} type="text"
                                                           placeholder={'Start typing..'}
                                                           onChange={onMessageInputChangeHandler}
                 onKeyPress={enterKeyDownHandler}/><span
-                    className={s.dialogs__send}><ArrowRight onClick={onSentButtonClick}/></span></div>
+                    className={s.dialogs__send}><ArrowRight  onClick={onSentButtonClick}/></span></div>
             </div>
+
         </div>
     )
 }
