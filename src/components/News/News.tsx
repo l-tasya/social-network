819142502200eabe52070e920ccfb87ea6e43008
l-tasya <dalionfull@gmail.com>
2021-12-31 18:09:58 +0600

    transform to dumb

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
 import {PostTextArea} from '../common/PostTextArea/PostTextArea';
-import {ActionsType, NewsPageType} from '../../redux/state';
+import {FeedPostsType, UserInfoType} from '../../redux/state';
 import s from './News.module.scss';
 import Post from '../Profile/MyPosts/Post/Post';
 import {Stories} from './Stories/Stories';
-import {addFeedPostAC, feedInputChangeAC} from '../../redux/feed-reducer';
 
 type NewsPropsType = {
-    dispatch: (action: ActionsType) => void
-    state: NewsPageType
+    feedNewPostText: string
+    onPostChange: (text: string)=>void
+    userInfo: UserInfoType
+    onItemAddClick: ()=>void
+    feedPost: FeedPostsType
 }
 
-const News: React.FC<NewsPropsType> = ({dispatch, state}) => {
+const News: React.FC<NewsPropsType> = (props) => {
     return (
         <div className={s.feed}>
             <Stories/>
-            <PostTextArea value={state.feedNewPostText}
-                          onChangeText={(text) => dispatch(feedInputChangeAC(text))}
-                          user={state.userInfo}
-                          addItem={() => dispatch(addFeedPostAC(1))}/>
+            <PostTextArea value={props.feedNewPostText}
+                          onChangeText={props.onPostChange}
+                          user={props.userInfo}
+                          addItem={props.onItemAddClick}/>
             {
-                state.feedPosts.map(t => <Post key={t.id} message={t.message} img={t.src} id={t.id}/>).reverse()
+                props.feedPost.map(t => <Post key={t.id} message={t.message} img={t.src} id={t.id}/>).reverse()
             }
         </div>
     )
