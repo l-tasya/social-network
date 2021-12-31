da37fa75e476d4d9041dcc0fda62e490f782c5f6
l-tasya <dalionfull@gmail.com>
2021-12-31 18:10:07 +0600

    transform to dumb

--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -1,31 +1,30 @@
 import React from 'react';
 import s from './MyPosts.module.scss'
 import Post from './Post/Post';
-import {ActionsType, ProfilePageType} from '../../../redux/state';
+import {PostsType, UserInfoType} from '../../../redux/state';
 import {PostTextArea} from '../../common/PostTextArea/PostTextArea';
-import {addPostAC, profileInputChangeAC} from '../../../redux/profile-reducer';
 
 
 type MyPostsPropsType = {
-    state: ProfilePageType
-    dispatch: (action: ActionsType) => void
+    post: PostsType
+    newPostText: string
+    userInfo: UserInfoType
+    addPost: ()=>void
+    onPostChange: (text: string)=>void
 }
 
-const MyPosts: React.FC<MyPostsPropsType> = ({state, dispatch}) => {
+const MyPosts: React.FC<MyPostsPropsType> = ({post,newPostText, userInfo, addPost, onPostChange}) => {
     //converting post state to Post component
-    let postElements = state.post.map((t) => <Post key={t.id} id={t.id} message={t.message} img={t.src}
+    let postElements = post.map((t) => <Post key={t.id} id={t.id} message={t.message} img={t.src}
     />).reverse()
-
-    //Flux circulation by textarea value
-
     return (
         <div className={s.myPosts}>
-                <PostTextArea
-                    value={state.newPostText}
-                    user={state.userInfo}
-                    addItem={()=>dispatch(addPostAC(1))}
-                    onChangeText={(text)=>dispatch(profileInputChangeAC(text))}
-                />
+            <PostTextArea
+                value={newPostText}
+                user={userInfo}
+                addItem={addPost}
+                onChangeText={onPostChange}
+            />
             <div className={s.myPosts__posts}>
                 {postElements}
             </div>
