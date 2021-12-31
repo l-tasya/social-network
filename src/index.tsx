70c41cc9aff647c055b9f08a3c2dc1defe434c59
l-tasya <dalionfull@gmail.com>
2021-12-31 18:13:20 +0600

    now we throw the store, not the state

--- src/index.tsx
+++ src/index.tsx
@@ -3,24 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {RootStateType} from './redux/state';
-import store from './redux/redux-store'
+import store, {ReduxStoreType} from './redux/redux-store'
 
 
-let rerenderEntireTree = (state: RootStateType,)=>{
+let rerenderEntireTree = (store: ReduxStoreType)=>{
     ReactDOM.render(
         <React.StrictMode>
             <App
-                state={state}
-                dispatch={store.dispatch.bind(store)}
-
+                store={store}
             />
         </React.StrictMode>,
         document.getElementById('root')
     );
 }
-rerenderEntireTree(store.getState())
-store.subscribe(()=>{rerenderEntireTree(store.getState())})
+rerenderEntireTree(store)
+store.subscribe(()=>{rerenderEntireTree(store)})
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
