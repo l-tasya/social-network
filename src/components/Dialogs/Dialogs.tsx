6a51ee7bdb6f0aedc874f271a56f99dd34b22b5f
l-tasya <dalionfull@gmail.com>
2022-01-04 19:02:01 +0600

    fix types import

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,24 +3,25 @@ import s from './Dialogs.module.scss';
 import Message from './Message/Message';
 import DialogsItem from './DialogsItem/DialogsItem';
 import {ArrowRight, MessageCircle} from 'react-feather';
-import {DialogsType, MessagesType} from '../../redux/state';
+import {DialogsType, MessagesType} from '../../redux/dialogs-reducer';
 
 type DialogsPropsType = {
     dialogItems: DialogsType
     dialogMessages: MessagesType
-    onDialogItemClick: ()=>void
+    onDialogItemClick: () => void
     newMessageText: string
-    error: string|null
-    onInputChange: (e: ChangeEvent<HTMLInputElement>)=>void
-    onEnterKeyPress: (e: React.KeyboardEvent)=>void
-    onSentButtonClick: ()=>void
+    error: string | null
+    onInputChange: (e: ChangeEvent<HTMLInputElement>) => void
+    onEnterKeyPress: (e: React.KeyboardEvent) => void
+    onSentButtonClick: () => void
 }
 
 
 const Dialogs: React.FC<DialogsPropsType> = (props) => {
 
     //converting state to component
-    let dialogElements = props.dialogItems.map((t) => <DialogsItem onDialogItemClick={props.onDialogItemClick} key={t.id} name={t.name} id={t.id}/>)
+    let dialogElements = props.dialogItems.map((t) => <DialogsItem onDialogItemClick={props.onDialogItemClick}
+                                                                   key={t.id} name={t.name} id={t.id}/>)
     let messageElements = props.dialogMessages.map((t) => <Message sent={t.sent} key={t.id} text={t.message}/>)
     return (
         <div className={s.dialogs}>
@@ -31,13 +32,13 @@ const Dialogs: React.FC<DialogsPropsType> = (props) => {
             <div className={s.dialogs__messages}>
                 {messageElements}
                 <div className={s.dialogs__typing}><input value={props.newMessageText}
-                                                          className={(props.error&&s.errorInput) + ' ' + s.dialogs__input} type="text"
+                                                          className={(props.error && s.errorInput) + ' ' + s.dialogs__input}
+                                                          type="text"
                                                           placeholder={'Start typing..'}
                                                           onChange={props.onInputChange}
-                onKeyPress={props.onEnterKeyPress}/><span
-                    className={s.dialogs__send}><ArrowRight  onClick={props.onSentButtonClick}/></span></div>
+                                                          onKeyPress={props.onEnterKeyPress}/><span
+                    className={s.dialogs__send}><ArrowRight onClick={props.onSentButtonClick}/></span></div>
             </div>
-
         </div>
     )
 }
