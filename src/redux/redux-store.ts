cf2aad2be1e607f2adcccd4735ea460e915dd3e1
l-tasya <dalionfull@gmail.com>
2022-01-04 18:51:51 +0600

    action types

--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -1,7 +1,17 @@
-import {combineReducers, createStore } from "redux";
-import {profileReducer} from './profile-reducer';
-import {feedReducer} from './feed-reducer';
-import {dialogsReducer} from './dialogs-reducer';
+import {combineReducers, createStore} from 'redux';
+import {addPostAC, profileInputChangeAC, profileReducer} from './profile-reducer';
+import {addFeedPostAC, feedInputChangeAC, feedReducer} from './feed-reducer';
+import {dialogInputChangeAC, dialogsReducer, fakeDialogsAC, sendMessageAC} from './dialogs-reducer';
+
+export type ActionsType =
+    ReturnType<typeof addPostAC>
+    | ReturnType<typeof sendMessageAC>
+    | ReturnType<typeof dialogInputChangeAC>
+    | ReturnType<typeof profileInputChangeAC>
+    | ReturnType<typeof fakeDialogsAC>
+    | ReturnType<typeof addFeedPostAC>
+    | ReturnType<typeof feedInputChangeAC>
+
 
 let reducers = combineReducers({
     profilePage: profileReducer,
@@ -11,4 +21,5 @@ let reducers = combineReducers({
 
 let store = createStore(reducers);
 export type ReduxStoreType = typeof store
+export type AppStateType = ReturnType<typeof reducers>
 export default store
