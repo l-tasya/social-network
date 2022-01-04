331deb5a4ceac5390aa35a75ab81ccaa812f7b6f
l-tasya <dalionfull@gmail.com>
2022-01-04 18:56:00 +0600

    fix profile types import

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import s from './Profile.module.scss';
 import Statistics from "./Statistics/Statistics";
 import UserInfo from "./UserInfo/UserInfo";
-import MyPostsContainer from './MyPosts/MyPostsContainer';
 import {ReduxStoreType} from '../../redux/redux-store';
+import MyPostsContainer from './MyPosts/MyPostsContainer';
 //types
 type ProfilePropsType = {
     store: ReduxStoreType
@@ -14,7 +14,7 @@ const Profile: React.FC<ProfilePropsType> = (props) => {
         <div className={s.content}>
             <UserInfo state={props.store.getState().profilePage.userInfo} />
             <Statistics />
-            <MyPostsContainer store={props.store}/>
+            <MyPostsContainer/>
         </div>
     );
 }
