26f471304586e7f33ab29bca0d0c2edb4ac3d095
l-tasya <dalionfull@gmail.com>
2022-01-04 19:05:51 +0600

    move types from old state.ts

--- src/redux/profile-reducer.ts
+++ src/redux/profile-reducer.ts
@@ -1,6 +1,29 @@
-import {ActionsType, PostType, ProfilePageType} from './state';
+import {ActionsType} from './redux-store';
+
 export let firstSrc = 'https://steamuserimages-a.akamaihd.net/ugc/97227892816512942/9D008E4EEFC6BFC6D3E283526BB6276393EA19F4/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false';
 export let secondSrc = 'https://static.turbosquid.com/Preview/001325/881/YD/_600.jpg';
+
+export type UserInfoType = {
+    imageSRC: string
+    name: string
+    surname: string
+    eMail: string
+}
+
+export type PostType = {
+    id: number
+    src: string
+    message: string
+}
+export type PostsType = Array<PostType>
+
+
+export type ProfilePageType = {
+    userInfo: UserInfoType
+    post: PostsType
+    newPostText: string
+}
+
 let initialState: ProfilePageType = {
     userInfo: {
         imageSRC: 'https://steamuserimages-a.akamaihd.net/ugc/97227892816512942/9D008E4EEFC6BFC6D3E283526BB6276393EA19F4/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false',
@@ -15,7 +38,7 @@ let initialState: ProfilePageType = {
     newPostText: '',
 }
 
-export const profileReducer = (state: ProfilePageType = initialState, action:ActionsType) =>{
+export const profileReducer = (state: ProfilePageType = initialState, action:ActionsType): ProfilePageType =>{
     if (action.type === 'ADD-POST') {
         let newPost: PostType = {
             id: state.post[state.post.length - 1].id + 1,
